f130e7303f9bc07477c35fdf55024ead2e711691
Kabilan SRK <kabilansrk@gmail.com>
2024-07-01 11:49:51 +0530

    Alteration based on Flask acceptible format.

--- app.py
+++ app.py
@@ -5,5 +5,40 @@ app = Flask(__name__)
 @app.route("/")
 def landing():
     return render_template("landing_page.html")
+def account():
+    return render_template("login-page.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+@app.route("/bookmark")
+def bookmark():
+    return render_template("bookmark.html")
+@app.route("/search")
+def search():
+    return render_template("search.html")
+@app.route("/feed")
+def feed():
+    return render_template("feed-from-search.html")
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+@app.route("/editprofile")
+def editprofile():
+    return render_template("edit-profile.html")
+@app.route("/lending")
+def lending():
+    return render_template("lending.html")
+@app.route("/message")
+def message():
+    return render_template("message.html")
+@app.route("/chat")
+def chat():
+    return render_template("message-1.html")
 if __name__ == "__main__":
     app.run(debug=True)
